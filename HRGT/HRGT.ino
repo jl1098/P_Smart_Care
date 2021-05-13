8bd4b014a6899ba8653ab928f4eccac97df45660
jl1098 <jl10.98@hotmail.com>
2021-05-13 14:02:17 -0500

    2

---
+++ HRGT/HRGT.ino
@@ -0,0 +1,24 @@
+//PROYECTO SMART CARE
+
+#include<ESP8266WiFi.h>
+#include<DNSServer.h>
+#include<ESP8266WebServer.h>
+#include <strings_en.h>
+#include <WiFiManager.h>
+#include "FirebaseESP8266.h"
+
+// Credenciales Proyecto Firebase
+const char *FIREBASE_HOST = "https://lampjlmv-default-rtdb.firebaseio.com/";
+const char *FIREBASE_AUTH = "iymtJuWfDIODWR9OmJxLtihH8YFBNnIDyFt0r1Tq";
+// Firebase Data object in the global scope
+FirebaseData firebaseData;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
